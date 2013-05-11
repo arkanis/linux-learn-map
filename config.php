81dd178959faeafab76918470382e8fe8dad7417
Stephan Soller <stephan.soller@helionweb.de>
2013-05-11 18:26:43 +0200

    Moved the CVS based user to server translation to check.php.

--- config.php
+++ config.php
@@ -1,23 +1,11 @@
 <?php
 
-$linuxvm_list = $ROOT_DIR . '/include/linuxvm-liste.csv';
-
-$row = 1;
-if (($handle = fopen($linuxvm_list, "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $row++;
-	if ($data[6] == $_SERVER['PHP_AUTH_USER']) {
-		$server_target = $data[0];
-		break;
-	}
-    }
-    fclose($handle);
-}
-
 $_CONFIG = array(
 	'tasks_dir' => $ROOT_DIR . '/exercises',
+	'ssh' => array(
+		'user_to_server_list' => $ROOT_DIR . '/user_to_server.csv'
+	),
 	'server_auth' => array(
-		'server' => $server_target,
 		'user' => 'root',
 		'public_key_file' => $ROOT_DIR . '/keys/test.pub',
 		'private_key_file' => $ROOT_DIR . '/keys/test'
--- public/check.php
+++ public/check.php
@@ -47,14 +47,36 @@ function run_script_on_server($script, $server, $user, $public_key_file, $privat
 	return array($output, $status_code);
 }
 
+// TODO: SANITIZE input!
 $exercise = $_GET['exercise'];
 $script = $_CONFIG['tasks_dir'] . '/' . $exercise . '/check';
-list($output, $status) = run_script_on_server($script, $_CONFIG['server_auth']['server'], $_CONFIG['server_auth']['user'], $_CONFIG['server_auth']['public_key_file'], $_CONFIG['server_auth']['private_key_file']);
+$logged_in_user = $_SERVER['PHP_AUTH_USER'];
+$server_name = null;
+
+// Lookup the server in the user to server list
+$fd = fopen($_CONFIG['ssh']['user_to_server_list'], 'r');
+if ($fd) {
+	while ( ($fields = fgetcsv($fd)) !== false ) {
+		@list($user, $server) = $fields;
+		if ($user === $logged_in_user) {
+			$server_name = $server;
+			break;
+		}
+	}
+	fclose($fd);
+}
+
+if (!$server_name) {
+	header('Content-Type: text/plain;charset=utf-8', true, 500);
+	echo("Sorry, couldn't check your exercise result. Don't know which virtual machine name belongs to your user name.");
+}
+
+list($output, $status) = run_script_on_server($script, $server_name, $_CONFIG['server_auth']['user'], $_CONFIG['server_auth']['public_key_file'], $_CONFIG['server_auth']['private_key_file']);
 
 if ($output === false or $status === false) {
 	// Couldn't run check script, return 500
 	header('Content-Type: text/plain;charset=utf-8', true, 500);
-	echo("Sorry, couldn't check your exercise result. Unable to open an SSH connection to your virtual machine.");
+	echo("Sorry, couldn't check your exercise result. Unable to open an SSH connection to your virtual machine $server_name.");
 } else {
 	$http_status = ($status === 0) ? 200 : 418;
 	header('Content-Type: text/plain;charset=utf-8', true, $http_status);
